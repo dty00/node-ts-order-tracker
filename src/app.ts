6e6c1aaac95046468ddd3bb206e1744ee4f5cfaa
dty00 <dingtianyi123@hotmail.com>
2026-07-10 15:27:16 +1000

    initial commit

---
+++ src/app.ts
@@ -0,0 +1,8 @@
+import{promises as fs} from "node:fs";
+
+// const data = fs.readFile("example.txt","utf8");
+// console.log("文件内容：", data);
+// // console.log(process.cwd());
+
+const filePath = "orderTrackLog.txt";
+
